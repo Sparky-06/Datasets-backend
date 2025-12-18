f06498bf58a13dce94af6c23ef7b0a8d234ca115
Spark-06 <aditya.kumarsharma2025@vitstudent.ac.in>
2025-12-18 16:58:33 +0530

    Backend skeletal

--- api/index.py
+++ api/index.py
@@ -3,9 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def root():
-    return {"Python": "on Vercel"}
+async def root():
+    return {"status": "ok"}
 
 @app.post("/anything")
-def post_anything():
-    return {"Python": "on Vercel"}
+async def anything():
+    return {"status": "posted"}
