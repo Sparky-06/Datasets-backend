efd0e3ac51633b7fc25afc014bece64d9b0eb32f
Spark-06 <aditya.kumarsharma2025@vitstudent.ac.in>
2025-12-19 12:49:54 +0530

    submission commit

--- api/ml/ml.py
+++ api/ml/ml.py
@@ -7,13 +7,6 @@ import requests
 EXTERNAL_API_URL = "https://smart-home-agent.onrender.com/state"
 
 def fetch_decision():
-    try:
         response = requests.get(EXTERNAL_API_URL, timeout=10)
         response.raise_for_status()
         return response.json()
-    except requests.exceptions.RequestException as e:
-        raise HTTPException(
-            status_code=502,
-            detail=f"Error fetching data from smart-home-agent: {str(e)}"
-        )
-print(fetch_decision())
